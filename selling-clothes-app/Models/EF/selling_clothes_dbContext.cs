49110cfcb3b3ccf930f15439feba6aa9bace0532
DangTuan1 <dangtuan1m69@gmail.com>
2023-09-07 23:40:24 +0700

    fix

--- selling-clothes-app/Models/EF/selling_clothes_dbContext.cs
+++ selling-clothes-app/Models/EF/selling_clothes_dbContext.cs
@@ -28,7 +28,7 @@ namespace selling_clothes_app.Models.EF
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=WINDOWS-10\\SQL1;Initial Catalog=selling_clothes_db;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=selling_clothes_db;Integrated Security=True");
             }
         }
 
